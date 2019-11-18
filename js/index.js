1ab3823048b25bdd3bcb0e174d32e4ff84f52aa1
Jorge Fabregas <jorge.fabregas22@gmail.com>
2019-11-17 21:13:18 -0500

    isrt commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+//var message = "Hello World!";// 
+//var message_two = "The First Script";//
+
+// var months = ['January', 'Febrary', 'March', 'April', 'May', 'Jun']; //
+
+// alert (message); //
+// alert (message_two); //
+
+// alert (months [0]); //
+// alert (months [1]); //
+// alert (months [2]); //
+// alert (months [3]); //
+// alert (months [4]); //
+// alert (months [5]); //
+
+var number1 = 5;
+var number2 = 8;
+
+if (number1 >= nunber2) {
+    alert ('Number 1 is not higher than number 2')
+};
